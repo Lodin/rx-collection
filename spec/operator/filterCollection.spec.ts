cad693ee6caee3ac2c9abebdca63bd4fae856349
Vlad Rindevich <rindevich.vs@gmail.com>
2017-06-25 19:01:29 +0300

    test(operator): move "filterCollection" tests to test helper functions

--- spec/operator/filterCollection.spec.ts
+++ spec/operator/filterCollection.spec.ts
@@ -1,208 +1,36 @@
 import 'rxjs/add/observable/of';
 import {Observable} from 'rxjs/Observable';
 import '../../src/add/operator/filterCollection';
-import {ArrayCollection, MapCollection, ObjectCollection, SetCollection} from '../../src/typings';
+import filterCollection from '../../src/operator/filterCollection';
+import {
+  filterArrayCollectionTestHelper,
+  filterMapCollectionTestHelper, filterObjectCollectionTestHelper,
+  filterSetCollectionTestHelper
+} from './utils/filterCollectionTestHelpers';
 
 describe('Operator "filterCollection"', () => {
-  describe('with map collection', () => {
-    let collection: MapCollection<string, number>;
+  describe('with map collection', filterMapCollectionTestHelper(filterCollection));
 
-    beforeEach(() => {
-      collection = Observable.of(new Map([['first', Observable.of(100)], ['second', Observable.of(200)]]));
-    });
+  describe('with set collection', filterSetCollectionTestHelper(filterCollection));
 
-    it('should filter', (done) => {
-      collection.filterCollection(value => value !== 100)
-        .subscribe((collection) => {
-          expect(collection.size).toBe(1);
+  describe('with array collection', filterArrayCollectionTestHelper(filterCollection));
 
-          const second = <Observable<number>>collection.get('second');
-          second.subscribe((value) => {
-            expect(value).toBe(200);
-            done();
-          });
-        });
-    });
+  describe('with object collection', filterObjectCollectionTestHelper(filterCollection));
 
-    it('should get key and collection along with value', (done) => {
-      let counter = 0;
-      collection.filterCollection((value, index, collection) => {
-        switch (counter) {
-          case 0:
-            expect(value).toEqual(100);
-            expect(index).toBe('first');
-            expect(collection).toEqual(jasmine.any(Map));
-            break;
-          case 1:
-            expect(value).toEqual(200);
-            expect(index).toBe('second');
-            expect(collection).toEqual(jasmine.any(Map));
-            done();
-            break;
-          default:
-            break;
-        }
-
-        counter += 1;
-
-        return value !== 100;
-      })
-        .subscribe((collection) => {
-          expect(collection).toEqual(jasmine.any(Map));
-        });
-    });
-  });
-
-  describe('with set collection', () => {
-    let collection: SetCollection<number>;
-
-    beforeEach(() => {
-      collection = Observable.of(new Set([Observable.of(100), Observable.of(200)]));
-    });
-
-    it('should filter', (done) => {
-      collection.filterCollection(value => value !== 100)
-        .subscribe((collection) => {
-          expect(collection.size).toBe(1);
-
-          const [second] = [...collection.values()];
-          second.subscribe((value) => {
-            expect(value).toBe(200);
-            done();
-          });
-        });
-    });
-
-    it('should get index and collection along with value', (done) => {
-      let counter = 0;
-      collection.filterCollection((value, index, collection) => {
-        switch (counter) {
-          case 0:
-            expect(value).toEqual(100);
-            expect(index).toBe(0);
-            expect(collection).toEqual(jasmine.any(Set));
-            break;
-          case 1:
-            expect(value).toEqual(200);
-            expect(index).toBe(1);
-            expect(collection).toEqual(jasmine.any(Set));
-            done();
-            break;
-          default:
-            break;
-        }
-
-        counter += 1;
-
-        return value !== 100;
-      })
-        .subscribe((collection) => {
-          expect(collection).toEqual(jasmine.any(Set));
-        });
-    });
-  });
-
-  describe('with array collection', () => {
-    let collection: ArrayCollection<number>;
-
-    beforeEach(() => {
-      collection = Observable.of([Observable.of(100), Observable.of(200)]);
-    });
-
-    it('should filter', (done) => {
-      collection.filterCollection(value => value !== 100)
-        .subscribe((collection) => {
-          expect(collection.length).toBe(1);
-
-          collection[0].subscribe((value) => {
-            expect(value).toBe(200);
-            done();
-          });
-        });
-    });
-
-    it('should get index and collection along with value', (done) => {
-      let counter = 0;
-      collection.filterCollection((value, index, collection) => {
-        switch (counter) {
-          case 0:
-            expect(value).toEqual(100);
-            expect(index).toBe(0);
-            expect(collection).toEqual(jasmine.any(Array));
-            break;
-          case 1:
-            expect(value).toEqual(200);
-            expect(index).toBe(1);
-            expect(collection).toEqual(jasmine.any(Array));
-            done();
-            break;
-          default:
-            break;
-        }
-
-        counter += 1;
-
-        return value !== 100;
-      })
-        .subscribe((collection) => {
-          expect(collection).toEqual(jasmine.any(Array));
-        });
-    });
-  });
-
-  describe('with object collection', () => {
-    let collection: ObjectCollection<number>;
-
-    beforeEach(() => {
-      collection = Observable.of({first: Observable.of(100), second: Observable.of(200)});
-    });
-
-    it('should filter', (done) => {
-      collection.filterCollection(value => value !== 100)
-        .subscribe((collection) => {
-          expect(Object.keys(collection).length).toBe(1);
-
-          collection.second.subscribe((value) => {
-            expect(value).toBe(200);
-            done();
-          });
-        });
-    });
-
-    it('should get key and collection along with value', (done) => {
-      let counter = 0;
-      collection.filterCollection((value, index, collection) => {
-        switch (counter) {
-          case 0:
-            expect(value).toEqual(100);
-            expect(index).toBe('first');
-            expect(collection).toEqual(jasmine.any(Object));
-            break;
-          case 1:
-            expect(value).toEqual(200);
-            expect(index).toBe('second');
-            expect(collection).toEqual(jasmine.any(Object));
-            done();
-            break;
-          default:
-            break;
-        }
-
-        counter += 1;
-
-        return value !== 100;
-      })
-        .subscribe((collection) => {
-          expect(collection).toEqual(jasmine.any(Object));
-        });
-    });
+  it('should work when added to Observable prototype', (done) => {
+    Observable.of([Observable.of(100), Observable.of(200)])
+      .filterCollection(value => value === 100)
+      .subscribe((collection) => {
+        expect(collection.length).toBe(1);
+        done();
+      });
   });
 
   it('should throw type error if the collection type is wrong', () => {
     expect(() => {
-      Observable.of(<any>1)
-        .filterCollection(value => value !== 100)
-        .subscribe(value => value);
+      filterCollection
+        .call(Observable.of(<any>1), (value: number) => value !== 100)
+        .subscribe((value: number) => value);
     }).toThrow(new TypeError('Unrecognized type of collection. Type should be "Array", "Object", "Map" or "Set"'));
   });
 });
---
+++ spec/operator/utils/filterCollectionTestHelpers.ts
@@ -0,0 +1,210 @@
+import 'rxjs/add/observable/of';
+import {Observable} from 'rxjs/Observable';
+import {
+  ArrayCollection, ArrayContent,
+  MapCollection, MapContent,
+  ObjectCollection, ObjectContent,
+  SetCollection, SetContent
+} from '../../../src/typings';
+
+export function filterMapCollectionTestHelper(operator: Function): () => void {
+  return () => {
+    let source: MapCollection<string, number>;
+
+    beforeEach(() => {
+      source = Observable.of(new Map([['first', Observable.of(100)], ['second', Observable.of(200)]]));
+    });
+
+    it('should filter collection', (done) => {
+      operator.call(source, (value: number) => value !== 100)
+        .subscribe((collection: MapContent<string, number>) => {
+          expect(collection.size).toBe(1);
+
+          const second = <Observable<number>>collection.get('second');
+          second.subscribe((value: number) => {
+            expect(value).toBe(200);
+            done();
+          });
+        });
+    });
+
+    it('should get key and collection along with value', (done) => {
+      let counter = 0;
+      operator.call(source, (value: number, index: string, collection: MapContent<string, number>) => {
+        switch (counter) {
+          case 0:
+            expect(value).toEqual(100);
+            expect(index).toBe('first');
+            expect(collection).toEqual(jasmine.any(Map));
+            break;
+          case 1:
+            expect(value).toEqual(200);
+            expect(index).toBe('second');
+            expect(collection).toEqual(jasmine.any(Map));
+            done();
+            break;
+          default:
+            break;
+        }
+
+        counter += 1;
+
+        return value !== 100;
+      })
+        .subscribe((collection: MapContent<string, number>) => {
+          expect(collection).toEqual(jasmine.any(Map));
+        });
+    });
+  };
+}
+
+export function filterSetCollectionTestHelper(operator: Function): () => void {
+  return () => {
+    let collection: SetCollection<number>;
+
+    beforeEach(() => {
+      collection = Observable.of(new Set([Observable.of(100), Observable.of(200)]));
+    });
+
+    it('should filter collection', (done) => {
+      operator.call(collection, (value: number) => value !== 100)
+        .subscribe((collection: SetContent<number>) => {
+          expect(collection.size).toBe(1);
+
+          const [second] = [...collection.values()];
+          second.subscribe((value) => {
+            expect(value).toBe(200);
+            done();
+          });
+        });
+    });
+
+    it('should get index and collection along with value', (done) => {
+      let counter = 0;
+      operator.call(collection, (value: number, index: number, collection: SetContent<number>) => {
+        switch (counter) {
+          case 0:
+            expect(value).toEqual(100);
+            expect(index).toBe(0);
+            expect(collection).toEqual(jasmine.any(Set));
+            break;
+          case 1:
+            expect(value).toEqual(200);
+            expect(index).toBe(1);
+            expect(collection).toEqual(jasmine.any(Set));
+            done();
+            break;
+          default:
+            break;
+        }
+
+        counter += 1;
+
+        return value !== 100;
+      })
+        .subscribe((collection: SetContent<number>) => {
+          expect(collection).toEqual(jasmine.any(Set));
+        });
+    });
+  };
+}
+
+export function filterArrayCollectionTestHelper(operator: Function): () => void {
+  return () => {
+    let collection: ArrayCollection<number>;
+
+    beforeEach(() => {
+      collection = Observable.of([Observable.of(100), Observable.of(200)]);
+    });
+
+    it('should filter collection', (done) => {
+      operator.call(collection, (value: number) => value !== 100)
+        .subscribe((collection: ArrayContent<number>) => {
+          expect(collection.length).toBe(1);
+
+          collection[0].subscribe((value: number) => {
+            expect(value).toBe(200);
+            done();
+          });
+        });
+    });
+
+    it('should get index and collection along with value', (done) => {
+      let counter = 0;
+      operator.call(collection, (value: number, index: number, collection: ArrayContent<number>) => {
+        switch (counter) {
+          case 0:
+            expect(value).toEqual(100);
+            expect(index).toBe(0);
+            expect(collection).toEqual(jasmine.any(Array));
+            break;
+          case 1:
+            expect(value).toEqual(200);
+            expect(index).toBe(1);
+            expect(collection).toEqual(jasmine.any(Array));
+            done();
+            break;
+          default:
+            break;
+        }
+
+        counter += 1;
+
+        return value !== 100;
+      })
+        .subscribe((collection: ArrayContent<number>) => {
+          expect(collection).toEqual(jasmine.any(Array));
+        });
+    });
+  };
+}
+
+export function filterObjectCollectionTestHelper(operator: Function): () => void {
+  return () => {
+    let collection: ObjectCollection<number>;
+
+    beforeEach(() => {
+      collection = Observable.of({first: Observable.of(100), second: Observable.of(200)});
+    });
+
+    it('should filter collection', (done) => {
+      operator.call(collection, (value: number) => value !== 100)
+        .subscribe((collection: ObjectContent<any>) => {
+          expect(Object.keys(collection).length).toBe(1);
+
+          collection.second.subscribe((value: number) => {
+            expect(value).toBe(200);
+            done();
+          });
+        });
+    });
+
+    it('should get key and collection along with value', (done) => {
+      let counter = 0;
+      operator.call(collection, (value: number, index: string, collection: ObjectContent<any>) => {
+        switch (counter) {
+          case 0:
+            expect(value).toEqual(100);
+            expect(index).toBe('first');
+            expect(collection).toEqual(jasmine.any(Object));
+            break;
+          case 1:
+            expect(value).toEqual(200);
+            expect(index).toBe('second');
+            expect(collection).toEqual(jasmine.any(Object));
+            done();
+            break;
+          default:
+            break;
+        }
+
+        counter += 1;
+
+        return value !== 100;
+      })
+        .subscribe((collection: ObjectContent<any>) => {
+          expect(collection).toEqual(jasmine.any(Object));
+        });
+    });
+  };
+}
