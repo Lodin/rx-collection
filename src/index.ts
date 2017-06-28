593ccfb9b4f113fc4cee87dffbc3d1a3e195e924
Vlad Rindevich <rindevich.vs@gmail.com>
2017-06-28 13:29:51 +0300

    feat: add index file

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import './add/observable/ofArrayCollection';
+import './add/observable/ofCollection';
+import './add/observable/ofMapCollection';
+import './add/observable/ofObjectCollection';
+import './add/observable/ofSetCollection';
+
+import './add/operator/filterArrayCollection';
+import './add/operator/filterCollection';
+import './add/operator/filterMapCollection';
+import './add/operator/filterObjectCollection';
+import './add/operator/filterSetCollection';
+
+import './add/operator/findInArrayCollection';
+import './add/operator/findInCollection';
+import './add/operator/findInMapCollection';
+import './add/operator/findInObjectCollection';
+import './add/operator/findInSetCollection';
+
+import './add/operator/forEachInArrayCollection';
+import './add/operator/forEachInCollection';
+import './add/operator/forEachInMapCollection';
+import './add/operator/forEachInObjectCollection';
+import './add/operator/forEachInSetCollection';
+
+export {default as SubjectMap} from './SubjectMap';
+export * from './typings';
